a548049c6ee3c131f99a56affa1f79cbcbd9841e
Carlos <carlos.mancilla993@gmail.com>
2024-01-19 10:52:24 -0500

    Hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "color-fondo": "var(--color-fondo)",
+        "color-titulo": "var(--color-titulo)",
+        "color-texto": "var(--color-texto)",
+        "color-primary": "var(--color-primary)",
+      },
+      fontFamily: {
+        sans: ["Raleway Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
